009606ef5c2217f7961fdc236453cfa00af31162
Victoria Kizzee <victoriakizzee93@gmail.com>
2024-06-25 14:21:40 -0500

    Create CaesarCipherEncryptor.py

---
+++ CaesarCipherEncryptor.py
@@ -0,0 +1,22 @@
+'''
+    Caesar Cipher Encryptor:
+    Given a string and a key to serve as an offset, encrypt the
+    string with a Caesar Cipher and return the new ciphertext.
+
+    Time:  O(N), where N = number of characters in the input string
+    Space: O(N), where N = number of characters in the input string
+
+'''
+def caesarCipherEncryptor(string, key):
+	key %= 26
+	ciphertext = []
+	for char in string:
+		ciphertext.append(encipher(char, key))
+	return "".join(ciphertext)
+
+def encipher(char, key):
+	start = ord('a') - 1
+	end = ord('z')
+	newChar = ord(char) + key
+	if newChar > end: return chr((newChar % end) + start)
+	return chr(newChar)
