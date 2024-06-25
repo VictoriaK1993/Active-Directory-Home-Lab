41aba63d73078f43149d63a4d5f9e6a8aa4547f8
Victoria Kizzee <victoriakizzee93@gmail.com>
2024-06-25 14:24:24 -0500

    Create FindClosestValueInBst.py

---
+++ FindClosestValueInBst.py
@@ -0,0 +1,31 @@
+'''
+    Find Closest Value In BST:
+    Given a BST as an input and a target value, return the
+    value of the node that is closest to the target value within
+    the BST
+
+    Best Time:  O(log(N)), where N = number of nodes in tree
+    Best Space: O(log(N)), where N = depth of recursive call stack
+    Worst Time/Space: O(N)/O(N), where no nodes have 2 children
+
+'''
+def findClosestValueInBst(tree, target):
+    return findClosest(tree, target, float("inf"))
+
+def findClosest(node, target, closest):
+	if node is None:
+		return closest
+	if abs(target - node.value) < abs(target - closest):
+		closest = node.value
+	if target < node.value:
+		return findClosest(node.left, target, closest)
+	elif target > node.value:
+		return findClosest(node.right, target, closest)
+	return closest
+	
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
