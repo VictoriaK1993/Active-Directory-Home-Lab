d2db7dd1975392999baccd74984bd068caf91e82
Victoria Kizzee <victoriakizzee93@gmail.com>
2024-06-25 14:38:59 -0500

    Create ReverseLinkedList.py

---
+++ ReverseLinkedList.py
@@ -0,0 +1,22 @@
+'''
+    Reverse Linked List:
+    Given a linked list, reverse it and return the new head node
+
+    Time:  O(N), where N are the number of nodes in the list
+    Space: O(3) -> O(1), three pointers are used for reversal operations
+
+'''
+class LinkedList:
+	def __init__(self, value):
+		self.value = value
+		self.next = None
+
+def reverseLinkedList(linkedList):
+	previousNode = None
+	currentNode = linkedList
+	while currentNode is not None:
+		nextNode = currentNode.next
+		currentNode.next = previousNode
+		previousNode = currentNode
+		currentNode = nextNode
+	return previousNode
